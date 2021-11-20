238a71d95e6e1ba5a2c778ba2868439fbcef63e4
labor_book <labor_book@10.211.55.12>
2021-11-20 13:26:20 +0900

    추가 수정 11월 20일

--- src/GitHub/Practice.java
+++ src/GitHub/Practice.java
@@ -5,6 +5,8 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		// Practice Connecting GitHub
+		
+		System.out.println("2021-11-20 수정");
 	}
 
 }
